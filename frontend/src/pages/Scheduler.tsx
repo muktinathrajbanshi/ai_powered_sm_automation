b10da70a72be2481ca808ba59affee56f7989570
unknown <muktinathrajbanshi786@gmail.com>
2026-06-03 19:58:22 +0545

    vok lago my lord

--- frontend/src/pages/Scheduler.tsx
+++ frontend/src/pages/Scheduler.tsx
@@ -1,18 +1,5 @@
-import { useState } from "react"
-
 const Scheduler = () => {
+  return <div></div>;
+};
 
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-
-  return (
-    <div className="flex h-screen bg-slate-50">
-
-        {/* Mobile Overlay  */}
-        { isMobileMenuOpen && <div className="fixed inset-0 bg-slate-900/50 z-40 md:hidden" /> }
-
-      Scheduler
-    </div>
-  )
-}
-
-export default Scheduler
+export default Scheduler;
