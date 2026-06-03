a148663f130334845726f7605b238b3788ec766b
unknown <muktinathrajbanshi786@gmail.com>
2026-06-03 21:12:33 +0545

    sahanai nasakne nidra haha

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,10 +1,23 @@
-
-const Sidebar = () => {
+const Sidebar = ({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: (val: boolean) => void;
+}) => {
   return (
-    <div>
-      Sidebar
+    <div
+      className={`fixed inset-y-0 left-0 z-50 w-64 bg-white border-r border-slate-200 flex flex-col h-full transform transition-transform duration-200
+         ease-in-out md:relative md:translate-x-0 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
+    >
+      {/* Logo  */}
+      <div className="p-6 pb-4">
+        <div className="text-xl tracking-tight text-slate-800 flex items-center gap-1.5">
+          <img src="/logo.svg" alt="logo" className="size-6" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
