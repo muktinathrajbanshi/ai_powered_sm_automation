aa4c2ff58b40436036973aab5905d45b727d6266
unknown <muktinathrajbanshi786@gmail.com>
2026-06-03 21:34:14 +0545

    khai ta green signal

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,3 +1,11 @@
+import {
+  CalendarDaysIcon,
+  LayoutDashboardIcon,
+  UserIcon,
+  Wand2Icon,
+} from "lucide-react";
+import { NavLink, useLocation } from "react-router-dom";
+
 const Sidebar = ({
   isOpen,
   setIsOpen,
@@ -5,6 +13,15 @@ const Sidebar = ({
   isOpen: boolean;
   setIsOpen: (val: boolean) => void;
 }) => {
+  const location = useLocation();
+
+  const navItems = [
+    { name: "Dashboard", icon: LayoutDashboardIcon, path: "/dashboard" },
+    { name: "Accounts", icon: UserIcon, path: "/accounts" },
+    { name: "Scheduler", icon: CalendarDaysIcon, path: "/schedule" },
+    { name: "AI Composer", icon: Wand2Icon, path: "/ai-composer" },
+  ];
+
   return (
     <div
       className={`fixed inset-y-0 left-0 z-50 w-64 bg-white border-r border-slate-200 flex flex-col h-full transform transition-transform duration-200
@@ -14,8 +31,25 @@ const Sidebar = ({
       <div className="p-6 pb-4">
         <div className="text-xl tracking-tight text-slate-800 flex items-center gap-1.5">
           <img src="/logo.svg" alt="logo" className="size-6" />
+          Scheduler
         </div>
       </div>
+
+      {/* Nav section label  */}
+      <div className="px-6 py-2">
+        <span className="text-xs text-slate-500 uppercase tracking-wider">
+          Menu
+        </span>
+      </div>
+
+      {/* Nav links  */}
+      <nav className="flex-1 px-3 space-y-1">
+        {navItems.map((item) => {
+          const isActive = location.pathname === item.path;
+
+          return <NavLink>{item.name}</NavLink>;
+        })}
+      </nav>
     </div>
   );
 };
