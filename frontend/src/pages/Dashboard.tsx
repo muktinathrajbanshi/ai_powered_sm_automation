589347c3224abe95cbe7cd7c0fa82839a8426214
unknown <muktinathrajbanshi786@gmail.com>
2026-06-04 17:57:42 +0545

    now actual role is started

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,10 +1,15 @@
-
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
+    <div className="space-y-8">
+      {/* Welcome bar  */}
+      <div>
+        <h2 className="text-2xl text-slate-900">Good morning! 👋</h2>
+        <p className="text-slate-500 text-sm mt-0.5">
+          Here's what's happening with your social accounts today.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
