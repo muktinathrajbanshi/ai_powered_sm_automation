5e1342accb0ccd9dbcb14498ddd2a7e82f07778f
unknown <muktinathrajbanshi786@gmail.com>
2026-06-05 16:45:33 +0545

    wah kiya baat hain

--- frontend/src/pages/Accounts.tsx
+++ frontend/src/pages/Accounts.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { PLATFORMS } from "../assets/assets";
+import { PlusIcon } from "lucide-react";
 
 const Accounts = () => {
   const [accounts, setAccounts] = useState<any[]>([]);
@@ -8,12 +10,20 @@ const Accounts = () => {
   return (
     <div className="space-y-8 max-w-4xl">
       {/* Header  */}
-      <div>
+      <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4 text-sm">
         <div>
-          <h2>Connected Accounts</h2>
-          <p>{accounts.length}</p>
+          <h2 className="text-xl text-slate-900">Connected Accounts</h2>
+          <p className="text-slate-500 text-sm mt-0.5">
+            {accounts.length} of {PLATFORMS.length} platforms connected
+          </p>
         </div>
-        <button></button>
+        <button
+          onClick={() => setShowPlatformPicker(true)}
+          className="flex items-center gap-2 px-5 py-2.5 bg-red-500 hover:bg-red-600
+        text-white rounded-full font-medium transition-all w-full sm:w-auto justify-center"
+        >
+          <PlusIcon className="size-4" /> Connect Account
+        </button>
       </div>
 
       {/* Platform picker modal  */}
