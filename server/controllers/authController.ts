94525546598ba3dc7129584d32ad47a98c6b8653
unknown <muktinathrajbanshi786@gmail.com>
2026-06-09 19:50:20 +0545

    vok lago my lord

---
+++ server/controllers/authController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
+// Register user
+// POST /api/auth/register
+
+export const registerUser = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ message: "User already exists" });
+      return;
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({ name, email, password: hashedPassword });
+
+    if (user) {
+      res
+        .status(201)
+        .json({ _id: user._id, name: user.name, email: user.email });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error?.message || "Server error" });
+  }
+};
