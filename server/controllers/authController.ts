e75316e438ef65da1e4b30647c30db81d047a156
unknown <muktinathrajbanshi786@gmail.com>
2026-06-09 21:39:54 +0545

    nidra lago my lord

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -30,16 +30,38 @@ export const registerUser = async (
     const user = await User.create({ name, email, password: hashedPassword });
 
     if (user) {
-      res
-        .status(201)
-        .json({
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          token: generateToken(user._id.toString()),
-        });
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id.toString()),
+      });
     } else {
       res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error?.message || "Server error" });
+  }
+};
+
+// Login user
+// POST /api/auth/login
+
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id.toString()),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
     }
   } catch (error: any) {
     res.status(500).json({ message: error?.message || "Server error" });
