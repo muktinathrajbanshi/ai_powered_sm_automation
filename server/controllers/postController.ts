640d0f0391e22818df2d682c2a6df173f68d20b7
unknown <muktinathrajbanshi786@gmail.com>
2026-06-11 18:08:25 +0545

    asli khel ta aba suru vayo yar

--- server/controllers/postController.ts
+++ server/controllers/postController.ts
@@ -1,12 +1,36 @@
 import { Response } from "express";
 import { AuthRequest } from "../middlewares/authMiddleware.js";
+import { GoogleGenAI } from "@google/genai";
 
 // Generate post
 // POST /api/posts/generate
 export const generatePost = async (
   req: AuthRequest,
   res: Response,
-): Promise<void> => {};
+): Promise<void> => {
+  try {
+    const { prompt, tone, generateImage } = req.body;
+
+    const apiKey = process.env.GEMINI_API_KEY;
+    if (!apiKey) {
+      res
+        .status(400)
+        .json({
+          message:
+            "Gemini API Key is missing. Please add it to your server/.env file.",
+        });
+      return;
+    }
+
+    const ai = new GoogleGenAI({ apiKey });
+
+    // Generate Text
+    const textResponse = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: ``,
+    });
+  } catch (error) {}
+};
 
 // Get generations
 // POST /api/posts/generations
